b23aea322d9e3a91d165b6659e529063e2fb9c45
KenTruong <anhkhoaprotruong@gmail.com>
2020-04-28 17:16:54 +0700

    a
    a

---
+++ Cau_Truc_Lenh_Co_Ban/Cau_Truc_Lenh_Co_Ban/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Cau_Truc_Lenh_Co_Ban
+{
+    class Program
+    {
+        /// <summary>
+        /// Hàm chạy đầu tiên khi chương trình được biên dịch
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            #region Console.Write()
+
+            Console.Write("Ken");  // In ra màng hình dòng chữ Ken
+            Console.Write(10);     // In ra màng hình số 10  
+            Console.Write(10.9f);  // In ra màng hình số 10.9 (f biểu thị cho kiểu dữ liệu float)
+            Console.Write(true);   // In ra màng hình chữ true của kiểu dữ liệ bool
+
+            Console.Write("Ken \n"); // Sử dụng ký tự đặc biệt để xuống dòng
+            Console.WriteLine(5);    // Sử dụng lệnh in ra màng hình có xuống dòng
+            Console.Write("Ken \n"); // In ra giá trị nhưng ko xuống dòng
+            Console.Write(Environment.NewLine); // Sử dụng lệnh xuống dòng
+            Console.Write("Ken \n"); // Sử dụng ký tự đặc biệt đẻ xuống dòng
+
+            Console.ReadLine(); // Đợi người dùng nhấn phím Enter
+            // ctrl + K + C -> cmt 
+            // ctrl + k + U -> mở cmt
+
+            #endregion
+
+            int a = 5; // Khai báo biến kiểu nguyên có tên là a và khởi tạo giá trị là 5
+            Console.Write("a = "); // In ra màng hình a =
+            Console.Write("a = " + a); // In ra màng hình giá trị a = 5
+            Console.Write("a = {0}" + a); // In ra màng hình giá trị a = 5
+            Console.Write(a); // In ra màng hình giá trị của a là 5
+            Console.Write("{0} {1} {2} {3} {4}", 1, 2, 3, 4, 5 + "\n");
+
+            Console.Write(Console.Read()); // đọc 1 ký tự từ bàn phím sau đó in ra ký tự vừa đọc bằng số nguyên
+            Console.Write(Console.ReadLine()); // đọc 1 ký tự từ bàn phím cho đến khi gập kí tự xuống dòng(enter) sau đó in ra ký tự vừa đọc bằng số nguyên
+
+
+            Console.ReadKey(); // Không truyền tham số vào thì mặc định là false
+            Console.ReadKey(false); // Hiển thị phím ấn lên màng hình
+            Console.ReadKey(true); // Không hiển thị phím ấn lên màng hình
+
+            Console.WriteLine("Ví dụ : ");
+            Console.Write("Mời bạn nhập tên của mình :");
+            Console.WriteLine("Tên của bạn là : " + Console.ReadLine());
+            // Quy tắc chung trong thực hiện lệnh là lệnh bên trong thục hiện trước rồi đến lệnh bên ngoài chứa nó
+            // Do đó khi chạy đến đây chương trình sẽ thực hiện lệnh Console.ReadLine()
+            // Sau đó thưc hiện lệnh cộng chuỗi cuối cùng in ra màng hình
+
+            Console.WriteLine("Mời bạn nhập ngày sinh : ");
+            Console.WriteLine("Ngày sinh của bạn là : " + Console.ReadLine());
+            Console.Write("Mời bạn nhập quê quán : ");
+            Console.WriteLine("Quê quán của bạn là : " + Console.ReadLine());
+
+
+
+            Console.ReadKey(); // Dừng màng hình để xem kết quả
+        }
+    }
+}
---
+++ DemoStruck/DemoStruck/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DemoStruck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // struck ko được kế thừa
+            // struck <tên struck>
+            // tên struck là tên kiểu dữ liệu do mình tự đặt ra
+            // Kiểu dữ liệu SinhVien có thể dùng làm kiểu dữ liệu cho biến , parametter cho phương thức .
+            // Ngoài ra còn có thể làm kiểu trả về cho phương thức
+            //vì struck là kiểu tham trị nên khi truyền vào các phương thức thì giá trị của nó sau khi kết thúc phương thức sẽ ko thay đổi.
+            // Do dó cần sử dụng từ khóa out đẻ có thể cập nhật giá trị thay đổi khi kết thúc phương thức .
+            SinhVien SV1 = new SinhVien();
+            Console.WriteLine("Nhap thong tin sinh vien");
+            NhapThongTinSinhVien(out SV1);
+            Console.WriteLine("**********");
+            Console.WriteLine("Thong Tin Sinh Vien Vua Nhap La :");
+            XuatThongTinSinhVien(SV1);
+            Console.WriteLine("diem Trung Binh Cua Sinh Vien Co Ma So " + SV1.MaSo + " la :" + DiemTrungBinhMon(SV1));
+            Console.WriteLine();
+        }
+        struct SinhVien
+        {
+            public int MaSo;
+            public string HoTen;
+            public double DiemToan;
+            public double DiemVan;
+            public double DiemAnhVan;
+        }
+        static void NhapThongTinSinhVien(out SinhVien SV)
+        {
+            Console.WriteLine("Nhap Ma so Sinh Vien");
+            SV.MaSo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap Ten Sinh Vien");
+            SV.HoTen = Console.ReadLine();
+            Console.WriteLine("Diem Toan");
+            SV.DiemToan = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Diem Van");
+            SV.DiemVan = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Diem Anh Van");
+            SV.DiemAnhVan = Double.Parse(Console.ReadLine());
+        }
+        static void XuatThongTinSinhVien(SinhVien SV)
+        {
+            Console.WriteLine("Ma So Sinh Vien" + SV.MaSo);
+            Console.WriteLine("Ho Ten Sinh Vien" + SV.HoTen);
+            Console.WriteLine("Diem Toan" + SV.DiemToan);
+            Console.WriteLine("Diem Van" + SV.DiemVan);
+            Console.WriteLine("Diem AnhVan" + SV.DiemAnhVan);
+        }
+        static double DiemTrungBinhMon(SinhVien SV)
+        {
+            return (SV.DiemToan + SV.DiemVan + SV.DiemAnhVan) / 3;
+        }
+    } 
+}
